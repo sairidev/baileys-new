dc2843cd0c45dbad74ee052abe4d5ba7dbcadf60
sairidev <akunfbkee14@gmail.com>
2026-05-31 16:21:22 +0700

    Delete Signal directory

--- Signal/Group/keyhelper.d.ts
+++
@@ -1,7 +0,0 @@
-export function generateSenderKey(): any;
-export function generateSenderKeyId(): any;
-export function generateSenderSigningKey(key: any): {
-    public: any;
-    private: any;
-};
-//# sourceMappingURL=keyhelper.d.ts.map
